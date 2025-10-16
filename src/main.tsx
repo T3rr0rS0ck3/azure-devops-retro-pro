007cde4bb4064fb726c4f5204f0d3ed1b5167fba
Robin Wessel <robinwessel1@gmail.com>
2025-10-16 19:31:53 +0200

    sdk fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import * as SDK from "azure-devops-extension-sdk";
+SDK.init();
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
