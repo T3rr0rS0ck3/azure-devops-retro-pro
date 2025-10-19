c4cb7cfc85b65331ce821b05413464c0389d9028
Robin Wessel <robinwessel1@gmail.com>
2025-10-19 10:21:12 +0200

    SDK INIT

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+(async () => {
+    const SDK = (window as any).VSS || (window as any).SDK;
+    SDK.init({ loaded: false });
+    await SDK.ready();
+    SDK.notifyLoadSucceeded();
+
+    ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+})();
