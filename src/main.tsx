c2a56bfe5075e296fae620a62092a3687223a394
Robin Wessel <robinwessel1@gmail.com>
2025-10-19 10:09:12 +0200

    sdk online

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import * as SDK from "azure-devops-extension-sdk";
 
-(window as any).__AzureSDK = SDK;
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
